a5ee3d5ca45be7e33bc7dd8442fc95a36c953bc1
hanyelC <hanyel.chamon@gmail.com>
2022-09-01 16:21:45 -0300

    run the migration when start the server

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ require('express-async-errors')
 
 const { port } = require('./configs/vars')
 const { app } = require('./configs/server')
+const { migrationsRun } = require('./database/sqlite/migrations')
+
+migrationsRun()
 
 app.listen(port, () => console.log(`server started at port ${port}`))
