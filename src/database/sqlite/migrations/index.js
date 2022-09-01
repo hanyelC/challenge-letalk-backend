ed92e222f13d82f96d1a936611b920083e515673
hanyelC <hanyel.chamon@gmail.com>
2022-09-01 15:49:46 -0300

    create database migration and connection

---
+++ src/database/sqlite/migrations/index.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+
+const sqliteConnection = require("../index")
+
+const createLoan = fs.readFileSync('./createLoan.sql').toString()
+
+async function migrationsRun() {
+  const schemas = [
+    createLoan
+  ].join('')
+
+  sqliteConnection()
+    .then(db => db.exec(schemas))
+    .catch(err => console.log(err))
+}
+
+module.exports = { migrationsRun }
